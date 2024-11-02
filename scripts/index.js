5afd4d0730f4ea5557772a1420fe63d55e75d549
Alexander Uvarov <alexander.uvarov001@gmail.com>
2024-11-02 23:11:57 +0300

    Выполненное задание

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector(`#card-template`).content;
+const placesList = document.querySelector(`.places__list`);
 
 // @todo: DOM узлы
+let cardElement = cardTemplate.querySelector(`.card`).cloneNode(true);
 
 // @todo: Функция создания карточки
+function cardCreating(data, deleteFunction) {
+    const cardElementClone = cardElement.cloneNode(true);
+    cardElementClone.querySelector(`.card__image`).src = data.link;
+    cardElementClone.querySelector(`.card__title`).textContent = data.name;
 
+    const deleteButton = cardElementClone.querySelector(`.card__delete-button`);
+    deleteButton.addEventListener(`click`, () => deleteFunction(cardElementClone));
+
+    return cardElementClone;
+}
 // @todo: Функция удаления карточки
+function cardDeleteFunction(card) {
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+    cardElement = cardCreating(item, cardDeleteFunction);
+    placesList.append(cardElement);
+})
