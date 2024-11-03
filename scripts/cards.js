e3298aa3ad0a49890900c6c015d0518f012e5a7a
Alexander Uvarov <alexander.uvarov001@gmail.com>
2024-11-03 11:06:35 +0300

    Исправление ошибок

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      altText: "Изображение гор Архыза, покрытых травой и участками со снегом",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      altText: "Река Челябинской области среди заснеженных берегов с лесом на фоне",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      altText: "Изображение панельных домов",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      altText: "Изображение поляны, находящеся на Камчатке, с редкими участками зелени и горой на фоне",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      altText: "Изображение железной дороги Холмогорского района, находящейся в лесу",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      altText: "Изображение холмистых берегов озера Байкал",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,13 @@ const cardTemplate = document.querySelector(`#card-template`).content;
 const placesList = document.querySelector(`.places__list`);
 
 // @todo: DOM узлы
-let cardElement = cardTemplate.querySelector(`.card`).cloneNode(true);
+const cardElement = cardTemplate.querySelector(`.card`).cloneNode(true);
 
 // @todo: Функция создания карточки
 function cardCreating(data, deleteFunction) {
     const cardElementClone = cardElement.cloneNode(true);
     cardElementClone.querySelector(`.card__image`).src = data.link;
+    cardElementClone.querySelector(`.card__image`).alt = data.altText;
     cardElementClone.querySelector(`.card__title`).textContent = data.name;
 
     const deleteButton = cardElementClone.querySelector(`.card__delete-button`);
@@ -23,6 +24,5 @@ function cardDeleteFunction(card) {
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (item) {
-    cardElement = cardCreating(item, cardDeleteFunction);
-    placesList.append(cardElement);
+    placesList.append(cardCreating(item, cardDeleteFunction));
 })
